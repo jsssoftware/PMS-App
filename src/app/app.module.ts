cfdc964201728da0d3e8129065dcf482fe53ad0f
Vivek <vivekparekh111@gmail.com>
2022-10-09 00:56:41 +0530

    move appcomponent from inside to out side

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app-modules/app/app.component';
+import { AppRoutingModule } from './app-routing.module'; 
+import { AppComponent } from './app.component'; 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
